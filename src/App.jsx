972fc69ffaf8c7ebac8086ddbeab488f1529df73
matikassssss <matias.farias@ug.uchile.cl>
2026-09-17 02:57:35 -0300

    feat(fix): extension mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import confetti from 'canvas-confetti'
 import { TURNS } from './constants'
 import { checkWinner, checkEndGame} from './logic/board'
